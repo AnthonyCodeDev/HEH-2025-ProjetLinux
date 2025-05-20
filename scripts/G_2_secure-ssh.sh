79c5f4bd8fe9e056154ed9daf6e7a10119b11d3c
Anthony <46421954+AnthonyCodeDev@users.noreply.github.com>
2025-05-20 12:19:22 +0200

    Update G_2_secure-ssh.sh

--- scripts/G_2_secure-ssh.sh
+++ scripts/G_2_secure-ssh.sh
@@ -73,7 +73,7 @@ enable_directive PermitRootLogin          no
 enable_directive PasswordAuthentication    yes
 enable_directive PubkeyAuthentication      yes
 enable_directive AuthorizedKeysFile        .ssh/authorized_keys
-enable_directive AllowUsers               "${ADMIN_USER} ec2-user root monitoring backup"
+enable_directive AllowUsers               "${ADMIN_USER} ec2-user monitoring backup"
 enable_directive X11Forwarding            no
 enable_directive AllowTcpForwarding       yes
 echo "UsePAM yes"                            >> "${SSH_CONFIG}"
